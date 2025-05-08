db0bf66b991ad1ebcc803b0e1d562b2b34bffb65
Can Onay <arifonay.853@gmail.com>
2025-05-08 20:17:03 +0300

    Gereksiz duran SatrancApp klasörü silindi ve at için gereklihamleler oluşturuldu

--- SatrancOyunu/SatrancApi/Entities/HamleSınıfları/AtHamlesi.cs
+++ SatrancOyunu/SatrancApi/Entities/HamleSınıfları/AtHamlesi.cs
@@ -1,4 +1,5 @@
-﻿using SatrancAPI.Entities.Models;
+﻿using SatrancApi.Entities.HamlelerinYonleri.AticinLHareketi;
+using SatrancAPI.Entities.Models;
 
 namespace SatrancAPI.Entities.HamleSınıfları
 {
@@ -6,7 +7,18 @@ namespace SatrancAPI.Entities.HamleSınıfları
     {
         public List<(int x, int y)> getGecerliHamleler(Tas tas, Tas[,] tahta)
         {
-            throw new NotImplementedException();
+        {
+            var hamleler = new List<(int x, int y)>();
+
+            // Önce geçerli hamle var mı kontrol ediyoruz
+            if (Lhareketi.GecerliMi(tas, tahta))
+            {
+                // Geçerli hamleler varsa onları hesaplıyoruz
+                hamleler.AddRange(Lhareketi.Hesapla(tas, tahta));
+            }
+
+            return hamleler;
         }
+    }
     }
 }
