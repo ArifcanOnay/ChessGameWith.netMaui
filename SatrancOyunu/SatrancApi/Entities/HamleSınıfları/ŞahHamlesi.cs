30b6963063b71b096c1409da511764ada64588c8
Can Onay <arifonay.853@gmail.com>
2025-05-11 22:32:22 +0300

    Nesne bağımlığını azaltmak için Hamle factory sınıfı oluşturuldu ve Şah hamlesi için yön sınıfı  oluşturuldu ve Rok hamlesinin entegreasyonu gerçekleştirildi.

--- SatrancOyunu/SatrancApi/Entities/HamleSınıfları/ŞahHamlesi.cs
+++ SatrancOyunu/SatrancApi/Entities/HamleSınıfları/ŞahHamlesi.cs
@@ -1,12 +1,28 @@
-﻿using SatrancAPI.Entities.Models;
+﻿using SatrancApi.Entities.HamlelerinYonleri.SahHamlesiYönleri;
+using SatrancApi.Entities.HamlelerinYonleri.SahHamlesiYonleri;
+using SatrancAPI.Entities.Models;
 
 namespace SatrancAPI.Entities.HamleSınıfları
 {
-    public class ŞahHamlesi : IHamle
+    public class SahHamlesi : IHamle
     {
-        public List<(int x, int y)> getGecerliHamleler(Tas tas, Tas[,] tahta)
-        {
-            throw new NotImplementedException();
+       
+            public List<(int x, int y)> getGecerliHamleler(Tas tas, Tas[,] tahta)
+            {
+                var hamleler = new List<(int x, int y)>();
+
+                // Şah her yönde sadece 1 kare hareket edebilir
+                hamleler.AddRange(TumYonler.Hesapla(tas, tahta));
+
+                //// Rök hamlesi için kontrol (ilave özellik)
+                 if (Rok.GecerliMi(tas, tahta))
+                 {
+                     hamleler.AddRange(Rok.Hesapla(tas, tahta));
+                 }
+
+                return hamleler;
+            }
         }
+
     }
-}
+
---
+++ SatrancOyunu/SatrancApi/Entities/HamlelerinYonleri/SahHamlesiYönleri/TumYönler.cs
@@ -0,0 +1,38 @@
+﻿using SatrancAPI.Entities.Models;
+
+namespace SatrancApi.Entities.HamlelerinYonleri.SahHamlesiYönleri
+{
+   
+        public class TumYonler
+        {
+            public static List<(int x, int y)> Hesapla(Tas tas, Tas[,] tahta)
+            {
+                List<(int x, int y)> hamleler = new List<(int x, int y)>();
+                int x = tas.X;
+                int y = tas.Y;
+
+                // 8 yön: sağ, sol, yukarı, aşağı, sağ-üst, sağ-alt, sol-üst, sol-alt
+                int[] xYonleri = { 1, -1, 0, 0, 1, 1, -1, -1 };
+                int[] yYonleri = { 0, 0, 1, -1, 1, -1, 1, -1 };
+
+                for (int yon = 0; yon < 8; yon++)
+                {
+                    int yeniX = x + xYonleri[yon];
+                    int yeniY = y + yYonleri[yon];
+
+                    if (yeniX >= 0 && yeniX < 8 && yeniY >= 0 && yeniY < 8)
+                    {
+                        if (tahta[yeniX, yeniY] == null || tahta[yeniX, yeniY].renk != tas.renk)
+                        {
+                            // Şah kontrolü yapılmalı - şahın tehdit altında olduğu kareye gidemez
+                            // Basitlik için şimdilik bu kontrolü atlıyoruz
+                            hamleler.Add((yeniX, yeniY));
+                        }
+                    }
+                }
+
+                return hamleler;
+            }
+        }
+    }
+
