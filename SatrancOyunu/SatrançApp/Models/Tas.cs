f0cd93629322c6c9763db8c4377c5c69a3f80719
Can Onay <arifonay.853@gmail.com>
2025-05-28 14:32:41 +0300

    Apidakiyle uyumlu entitiy sınıflarını eklendi ve Api ile iletişime geçmesi için servis sınıfları oluşturuldu

---
+++ SatrancOyunu/SatrançApp/Models/Tas.cs
@@ -0,0 +1,35 @@
+﻿// Models/Tas.cs - Satranç taşı bilgilerini tutuyor
+using System.Text.Json.Serialization;
+
+namespace ChessGameMaui.Models
+{
+    public class Tas
+    {
+        [JsonPropertyName("tasId")]
+        public Guid TasId { get; set; }
+
+        [JsonPropertyName("oyunId")]
+        public Guid OyunId { get; set; }
+
+        [JsonPropertyName("oyuncuId")]
+        public Guid OyuncuId { get; set; }
+
+        [JsonPropertyName("renk")]
+        public Renk Renk { get; set; }
+
+        [JsonPropertyName("turu")]
+        public TasTuru Turu { get; set; }
+
+        [JsonPropertyName("x")]
+        public int X { get; set; }
+
+        [JsonPropertyName("y")]
+        public int Y { get; set; }
+
+        [JsonPropertyName("aktifMi")]
+        public bool AktifMi { get; set; }
+
+        [JsonPropertyName("hicHareketEtmediMi")]
+        public bool HicHareketEtmediMi { get; set; }
+    }
+}
